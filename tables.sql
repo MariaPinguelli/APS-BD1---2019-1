a5c190409f3aaf9625b5108dc6fac6be3cf6e370
MariaPinguelli <mariaferpinguelli@hotmail.com>
2019-06-24 09:42:39 -0300

    teste

--- tables.sql
+++ tables.sql
@@ -3,23 +3,28 @@ DROP TABLE IF EXISTS  CLIENTE;
 DROP TABLE IF EXISTS  PEDIDO;
 
 CREATE TABLE FLOR(
+    id INTEGER AUTO_INCREMENT NOT NULL,
     nome VARCHAR,     /* margaridas, rosas, orquídsas etc */
-    tipo VARCHAR (100),     
     corFlor VARCHAR (100),
     qtdeEstoque INTEGER,
     precoUnit VALUE,
-    PRIMARY KEY (nome)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE CLIENTE(
+    id INTEGER AUTO_INCREMENT NOT NULL,
     nroCliente INTEGER,
     nomeCliente VARCHAR (100),
-    PRIMARY KEY (nroCliente)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE PEDIDO(
-    nroCliente INTEGER,
+    idCliente INTEGER,
+    idFlor INTEGER,
+    qtd INTEGER,
+    precoTotal FLOAT,
     destino VARCHAR (100),
-    PRIMARY KEY (nroCliente),
-    FOREIGN KEY (nroCliente) REFERENCES CLIENTE (nroCliente)
+    FOREIGN KEY (idCliente) REFERENCES CLIENTE(id),
+    FOREIGN KEY (idFlor) REFERENCES FLOR(id),
+    PRIMARY KEY (idCliente, idFlor)
 );
